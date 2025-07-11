b23d306998edf398e83d73da4290869764b27598
baird-a11y <pbaselt@students.uni-mainz.de>
2025-07-11 10:54:53 +0200

    Add initial implementation of Count Down program in Scala

---
+++ src/Blatt_1/Count_Down.scala
@@ -0,0 +1,10 @@
+// Count Down Scala
+@main def countDown = {       // Syntax kept close to JAVA 
+  for (i <- 10 to 1 by -1) {           // (for simplicity) 
+    println(i); 
+    if (i == 8) { 
+        println("ignition sequence start"); 
+    } 
+  } 
+  println("liftoff!"); 
+}
