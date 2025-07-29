fe4c243fca1b1dd0717de6f4780c4a32f15ef798
Baselt, Paul <baselt@uni-mainz.de>
2025-07-29 10:54:06 +0200

    Add initial implementation of exam_prep.py with PySide6 GUI components

---
+++ Aufgaben/exam_prep.py
@@ -0,0 +1,8 @@
+from PySide6.QtCore import Qt 
+from PySide6.QtGui import QKeySequence, QPixmap, QCloseEvent, QImage 
+from PySide6.QtGui import QPainter, QColor 
+from PySide6.QtWidgets import QApplication, QMenuBar, QToolBar 
+from PySide6.QtWidgets import QWidget, QFileDialog 
+from PySide6.QtWidgets import QMainWindow, QMessageBox 
+
+
